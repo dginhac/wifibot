8759c3b215802916502012a4f7f81076016d00f6
d0m G <dom@ginhac.com>
2021-04-28 11:58:06 +0200

    initial commit

---
+++ qt/README.md
@@ -0,0 +1,2 @@
+# wifibotlab
+simple C++ Class that implements Wifibot v3 communication protocol using QT
---
+++ qt/myrobot.cpp
@@ -0,0 +1,71 @@
+// myrobot.cpp
+
+#include "myrobot.h"
+
+MyRobot::MyRobot(QObject *parent) : QObject(parent) {
+    DataToSend.resize(9);
+    DataToSend[0] = 0xFF;
+    DataToSend[1] = 0x07;
+    DataToSend[2] = 0x0;
+    DataToSend[3] = 0x0;
+    DataToSend[4] = 0x0;
+    DataToSend[5] = 0x0;
+    DataToSend[6] = 0x0;
+    DataToSend[7] = 0x0;
+    DataToSend[8] = 0x0;
+    DataReceived.resize(21);
+    TimerEnvoi = new QTimer();
+    // setup signal and slot
+    connect(TimerEnvoi, SIGNAL(timeout()), this, SLOT(MyTimerSlot())); //Send data to wifibot timer
+}
+
+
+void MyRobot::doConnect() {
+    socket = new QTcpSocket(this); // socket creation
+    connect(socket, SIGNAL(connected()),this, SLOT(connected()));
+    connect(socket, SIGNAL(disconnected()),this, SLOT(disconnected()));
+    connect(socket, SIGNAL(bytesWritten(qint64)),this, SLOT(bytesWritten(qint64)));
+    connect(socket, SIGNAL(readyRead()),this, SLOT(readyRead()));
+    qDebug() << "connecting..."; // this is not blocking call
+    //socket->connectToHost("LOCALHOST", 15020);
+    socket->connectToHost("192.168.1.106", 15020); // connection to wifibot
+    // we need to wait...
+    if(!socket->waitForConnected(5000)) {
+        qDebug() << "Error: " << socket->errorString();
+        return;
+    }
+    TimerEnvoi->start(75);
+
+}
+
+void MyRobot::disConnect() {
+    TimerEnvoi->stop();
+    socket->close();
+}
+
+void MyRobot::connected() {
+    qDebug() << "connected..."; // Hey server, tell me about you.
+}
+
+void MyRobot::disconnected() {
+    qDebug() << "disconnected...";
+}
+
+void MyRobot::bytesWritten(qint64 bytes) {
+    qDebug() << bytes << " bytes written...";
+}
+
+void MyRobot::readyRead() {
+    qDebug() << "reading..."; // read the data from the socket
+    DataReceived = socket->readAll();
+    emit updateUI(DataReceived);
+    qDebug() << DataReceived[0] << DataReceived[1] << DataReceived[2];
+}
+
+void MyRobot::MyTimerSlot() {
+    qDebug() << "Timer...";
+    while(Mutex.tryLock());
+    socket->write(DataToSend);
+    Mutex.unlock();
+}
+
