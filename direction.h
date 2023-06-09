e3d0eba823b2b5f53dce29d88ab01bdd7413e19d
Juliette <73775361+Julietteg36@users.noreply.github.com>
2023-06-09 11:28:48 +0200

    The robot can now move (direction.h, move function)

---
+++ direction.h
@@ -0,0 +1,16 @@
+#ifndef DIRECTION_H
+#define DIRECTION_H
+
+enum class Direction {
+    FORWARD,
+    FORWARD_LEFT,
+    FORWARD_RIGHT,
+    LEFT,
+    RIGHT,
+    BACKWARD,
+    BACKWARD_LEFT,
+    BACKWARD_RIGHT,
+    NONE
+};
+
+#endif // DIRECTION_H
